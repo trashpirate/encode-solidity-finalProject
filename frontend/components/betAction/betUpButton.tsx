0ec65b77f5d53830de43a2487d5f3a96033ae114
Danny Trejo <danieltrejo112@gmail.com>
2023-09-12 16:40:13 -0500

    Added wagmi contract interactions

--- frontend/components/betAction/betUpButton.tsx
+++ frontend/components/betAction/betUpButton.tsx
@@ -1,26 +1,17 @@
 import styles from "./betAction.module.css";
-
-import { tokenABI } from '@/assets/MyERC20Token';
 import { bettingABI } from "@/assets/Betting";
-
 import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
-import { parseUnits } from "viem";
+
 
 const TOKEN_CONTRACT = process.env.NEXT_PUBLIC_TOKEN_CONTRACT as `0x${string}`;
 const BETTING_CONTRACT = process.env.NEXT_PUBLIC_BETTING_ADDRESS as `0x${string}`;
-// const UP_ADDRESS = process.env.NEXT_PUBLIC_UP_ADDRESS as `0x${string}`;
-// const DOWN_ADDRESS = process.env.NEXT_PUBLIC_DOWN_ADDRESS as `0x${string}`;
 const NETWORK_SCAN = process.env.NEXT_PUBLIC_NETWORK_SCAN;
 
-// export default function BetButton(params: { action: string; transferAmount: string }) {
-export default function BetButton() {
-  // const walletAddress = params.action === "UP" ? UP_ADDRESS : DOWN_ADDRESS;
-
+export default function BetUpButton() {
   const { config } = usePrepareContractWrite({
     address: BETTING_CONTRACT as `0x${string}`,
     abi: bettingABI,
-    functionName: "bet",
-    // args: [walletAddress as `0x${string}`, parseUnits(`${Number(params.transferAmount)}`, 18)],
+    functionName: "betUp",
   });
   const { data, error, isError, write } = useContractWrite(config);
 
@@ -29,26 +20,16 @@ export default function BetButton() {
     hash: data?.hash,
   });
 
-  // const getButtonColor = () => {
-  //   if (params.action === "UP") {
-  //     return { background: "rgb(14, 207, 143)" };
-  //   } else {
-  //     return { background: "rgb(240, 31, 94)" };
-  //   }
-  // };
-
   return (
     <div>
       <div>
         <button
           className={styles.button}
-          // style={getButtonColor()}
           style={{ background: "rgb(14, 207, 143)"}}
           disabled={!write || isLoading}
           onClick={() => write?.()}
         >
-          {/* {isLoading ? "Submitting..." : `Bet ${params.action}`} */}
-          {isLoading ? "Submitting..." : `Bet`}
+          {isLoading ? "Submitting..." : `Bet Up`}
         </button>
       </div>
       {isSuccess && (
--- frontend/components/betAction/claimButton.tsx
+++ frontend/components/betAction/claimButton.tsx
@@ -1,26 +1,19 @@
 import styles from "./betAction.module.css";
-
-import { tokenABI } from '@/assets/MyERC20Token';
 import { bettingABI } from "@/assets/Betting";
-
-import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
 import { parseUnits } from "viem";
+import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
+
 
 const TOKEN_CONTRACT = process.env.NEXT_PUBLIC_TOKEN_CONTRACT as `0x${string}`;
 const BETTING_CONTRACT = process.env.NEXT_PUBLIC_BETTING_ADDRESS as `0x${string}`;
-// const UP_ADDRESS = process.env.NEXT_PUBLIC_UP_ADDRESS as `0x${string}`;
-// const DOWN_ADDRESS = process.env.NEXT_PUBLIC_DOWN_ADDRESS as `0x${string}`;
 const NETWORK_SCAN = process.env.NEXT_PUBLIC_NETWORK_SCAN;
 
-// export default function BetButton(params: { action: string; transferAmount: string }) {
-export default function BetButton() {
-  // const walletAddress = params.action === "UP" ? UP_ADDRESS : DOWN_ADDRESS;
-
+export default function ClaimButton(params: {withdrawlAmount: string}) {
   const { config } = usePrepareContractWrite({
     address: BETTING_CONTRACT as `0x${string}`,
     abi: bettingABI,
-    functionName: "bet",
-    // args: [walletAddress as `0x${string}`, parseUnits(`${Number(params.transferAmount)}`, 18)],
+    functionName: "claimPrize",
+    args: [parseUnits(`${params.withdrawlAmount ?? '0'}`, 18)],
   });
   const { data, error, isError, write } = useContractWrite(config);
 
@@ -29,26 +22,16 @@ export default function BetButton() {
     hash: data?.hash,
   });
 
-  // const getButtonColor = () => {
-  //   if (params.action === "UP") {
-  //     return { background: "rgb(14, 207, 143)" };
-  //   } else {
-  //     return { background: "rgb(240, 31, 94)" };
-  //   }
-  // };
-
   return (
     <div>
       <div>
         <button
           className={styles.button}
-          // style={getButtonColor()}
           style={{ background: "rgb(14, 207, 143)"}}
           disabled={!write || isLoading}
           onClick={() => write?.()}
         >
-          {/* {isLoading ? "Submitting..." : `Bet ${params.action}`} */}
-          {isLoading ? "Submitting..." : `Bet`}
+          {isLoading ? "Submitting..." : `Claim Prize`}
         </button>
       </div>
       {isSuccess && (
